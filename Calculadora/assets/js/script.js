3944d2193a217ce7c3db9da747eb18dccf026acb
Esteban Turrys <dr.turrys@gmail.com>
2023-05-09 20:59:44 -0400

    se modifica calculadora div por button

---
+++ Calculadora/assets/js/script.js
@@ -0,0 +1,4 @@
+const pantalla = document.getElementById("text-box");
+const botonesNumeros = document.querySelectorAll('#numero')
+const botonesOperadores = document.querySelectorAll('#operador')
+
